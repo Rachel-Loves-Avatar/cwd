e2a4b04c0c17da9a6023232b46cf6ff831272d05
Rachel-Loves-Avatar <rtubbs2003@gmail.com>
2026-04-03 23:37:01 -0400

    usability test draft

---
+++ unit-4-usability/assets/hello.js
@@ -0,0 +1,7 @@
+
+let myForm = document.querySelector("#myForm");
+
+myForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+	console.log(this.msg.value);
+});
---
+++ unit-4-usability/assets/usability.js
@@ -0,0 +1,6 @@
+let myForm = document.querySelector("#myForm");
+
+myForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+	console.log(this.msg.value);
+});
